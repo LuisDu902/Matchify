05685037a87533a2d0e92800ad0711427a731d2b
José Santos <santos.josemssantosrgmail.com>
2023-04-27 20:41:55 +0100

    Merge Unit Test Fix

--- app/test/swipe_test.dart
+++ app/test/swipe_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:matchify/main.dart';
+import 'package:matchify/song/song.dart';
 import 'package:matchify/song/swipe.dart';
+import 'package:flutter/material.dart';
 
 
 void main() {
@@ -9,14 +11,13 @@ void main() {
     test('fetchSong returns list of songs for 2 genres', () async {
       runApp(MyApp());
       final swipePage = SwipePage();
-      final swipeState = swipePage.createState();
       final genre1 = 'Rock';
       final genre2 = 'Pop';
       List<String> filters=[];
       filters.add(genre1);
       filters.add(genre2);
-      await swipeState.fetchSongs(filters);
-      expect(swipeState.songs.length,2);
+      await fetchSongs(filters);
+      expect(songs.length,2);
     });
 
     test('fetchSong returns only music from specific genre',() async{
@@ -28,11 +29,38 @@ void main() {
       List<String> filters=[];
       filters.add(genre1);
       filters.add(genre2);
-      await swipeState.fetchSongs(filters);
-      for(int i=0;i< swipeState.songs.length;i++){
-        expect(filters.contains(swipeState.songs[i].genre),true );
+      songs.clear();
+      await fetchSongs(filters);
+      for(int i=0;i< songs.length;i++){
+        expect(filters.contains(songs[i].genre),true );
       }
     });
+
+   /*testWidgets('Test Dismissible Widget', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: SwipePage()));
+      final SwipeState swipeState= tester.state<SwipeState>(find.byType(SwipePage));
+   
+      var dismissibleFinder =  find.byWidget(Dismissible(key: UniqueKey(), child: Align()));
+      expect(dismissibleFinder, findsOneWidget);
+
+      var songIndex = 0;
+      var currentSong = swipeState.songs[songIndex];
+
+      await tester.drag(dismissibleFinder, Offset(1000.0, 0.0));
+      await tester.pumpAndSettle();
+
+    // Check if the song is added to the disliked list
+      expect(getDislikedSongs().contains(currentSong),true);
+
+      songIndex++;
+      currentSong = swipeState.songs[songIndex];
+
+      await tester.drag(dismissibleFinder, Offset(-1000.0, 0.0));
+      await tester.pumpAndSettle();
+
+      // Check if the song is added to the liked list
+      expect(getLikedSongs().contains(currentSong), true);
+  });*/
   });
 }
 
